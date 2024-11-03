9a8bbdf1759a95970327f99dc7c662fafb2d64a9
KubangPawis <xanderfernandezperegrin@gmail.com>
2024-11-03 21:26:43 +0800

    feat: add title page text animation

--- pages/home.py
+++ pages/home.py
@@ -6,8 +6,8 @@ df = pd.read_csv('./data/electric_vehicle_pop_clean.csv')
 home_layout = [
         html.Div([
             html.Div([
-                html.Div('Electric Vehicle Population Data Analysis', className='title-div home-title-div'),
-                html.Div('A data storytelling app', className='title-div home-subtitle-div'),
+                html.Div('Electric Vehicle Population Data Analysis', className='title-div home-title-div animate__animated animate__bounceIn'),
+                html.Div('A data storytelling app', className='title-div home-subtitle-div animate__animated animate__bounceIn'),
                 dcc.Link('Start Story',href='/story1_1', className='story-next-btn change-color-hover')
             ], className='home-wrapper')
         ])
